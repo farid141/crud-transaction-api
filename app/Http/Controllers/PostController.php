3063adf3bd73135a97790972347f7e62a7a74a2e
farid <faridnubaili@gmail.com>
2025-01-15 20:52:01 +0700

    feature: crud posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        DB::beginTransaction();
+        $validated = $request->validated();
+        $validated['created_by_id'] = Auth::id();
+
+        $post = Post::create($validated);
+
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $image) {
+                $post->addMedia($image)->toMediaCollection('images');
+            }
+        }
+        DB::commit();
+
+        return response()->json([
+            'message' => 'Post created successfully',
+            'post' => $post->setHidden(['media']),
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        $post['images'] = $post->getMedia('images')->map(function ($image) {
+            return $image->getUrl();
+        });
+        return response()->json($post->makeHidden(['media']));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        DB::beginTransaction();
+        $validated = $request->validated();
+
+        $post->getMedia('images')->each->delete();
+
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $image) {
+                $post->addMedia($image)->toMediaCollection('images');
+            }
+        }
+
+        $post->update($validated);
+        $post->save();
+        DB::commit();
+
+        return response()->json([
+            'message' => 'Post updated successfully',
+            'post' => $post->setHidden(['media']),
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        DB::beginTransaction();
+        $post->getMedia('images')->each->delete();
+        $post->delete();
+        DB::commit();
+
+        return response()->json([
+            'message' => 'Post deleted successfully',
+            'post' => $post->setHidden(['media']),
+        ], 201);
+    }
+}
---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['string', Rule::unique('posts', 'title')->ignore($this->post->id)],
+            'content' => 'string',
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation failed',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
